0d346f29217d8edee3f252972cc518e3ba9940a0
natafajardo <79486039+natafajardo@users.noreply.github.com>
2021-05-23 11:18:33 -0500

    creación de página quienes somos

--- src/app/pages/quienes/quienes.component.ts
+++ src/app/pages/quienes/quienes.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuienesComponent implements OnInit {
 
+  teams = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.teams = [
+      {
+        name: "Carolina Guacheta",
+        role: "Lider Tecnico",
+        img: "../../../assets/img/quienes/diana.jpg"
+      },
+      {
+        name: "Natalia Fajardo",
+        role: "Product owner",
+        img: "../../../assets/img/quienes/natalia.jpg"
+      },
+      {
+        name: "Jonathan Maldonado",
+        role: "Scrum Master",
+        img: "../../../assets/img/quienes/jonathan.jpg"
+      },
+      {
+        name: "Andres Florido",
+        role: "Scrum Team",
+        img: "../../../assets/img/quienes/andres.jpg"
+      },
+      {
+        name: "Yoneider Lopez",
+        role: "Scrum Team",
+        img: "../../../assets/img/quienes/yoneider.jpg"
+      },
+      {
+        name: "Layoner Murgas",
+        role: "Scrum Team",
+        img: "../../../assets/img/quienes/layo.jpg"
+      },
+    ]
   }
 
 }
